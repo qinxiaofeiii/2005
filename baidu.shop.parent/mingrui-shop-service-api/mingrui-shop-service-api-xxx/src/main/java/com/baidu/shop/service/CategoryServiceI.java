8f5791249235f90041ee4c454bc16e299555a53e
qinxiaofeiii <qinxiaofeiii@163.com>
2021-01-20 18:55:48 +0800

    2021.1.20号商品分类CRUD

---
+++ baidu.shop.parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/CategoryServiceI.java
@@ -0,0 +1,31 @@
+package com.baidu.shop.service;
+
+import com.baidu.shop.base.Result;
+import com.baidu.shop.entity.CategoryEntity;
+import com.google.gson.JsonObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api (tags = "商品分类接口")
+public interface CategoryServiceI {
+
+    @ApiOperation(value="通过pid查询商品分类")
+    @GetMapping(value = "category/list")
+    Result<List<CategoryEntity>> getCategoryListByPid(Integer pid);
+
+    @ApiOperation(value = "通过id删除商品分类")
+    @DeleteMapping(value = "/category/delete")
+    Result<JsonObject> deleteCategoryById(Integer id);
+
+    @ApiOperation(value = "通过id更新商品分类name")
+    @PutMapping(value = "/category/update")
+    Result<JsonObject> updateCategoryNameById(@RequestBody CategoryEntity categoryEntity);
+
+    @ApiOperation(value = "新增商品分类")
+    @PostMapping(value = "/category/save")
+    Result<JsonObject> saveCategoryNameById(@RequestBody CategoryEntity categoryEntity);
+}
